79f11c12ce50734161eacbf0c5633a926d837c75
Patrícia Carbri <patriciacarbri@gmail.com>
2022-08-16 17:24:31 -0300

    Correção da pasta

---
+++ javaScriptBase/codTeste/boolean.js
@@ -0,0 +1,25 @@
+// const primeiroNumero = 5;
+// const segundoNumero = 10;
+
+// const comparaNumero = primeiroNumero == segundoNumero
+
+// console.log(comparaNumero);
+
+// truthy ou falsy
+
+// 0 == false
+// 1 == true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true)
+
+// null == vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+console.log(typeof minhaVar);
+console.log(typeof varNull);
